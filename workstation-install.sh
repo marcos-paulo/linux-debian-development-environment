b2481a9553633a538a9f0b9bfc10e2d2e1d64568
Marcos Paulo <marcospaulo.mpa@gmail.com>
2022-09-06 16:51:18 -0300

    :sparkles: Modified: workstation-install.sh, add new scripts

--- workstation-install.sh
+++ workstation-install.sh
@@ -1,35 +1,84 @@
 #!/bin/bash
+source ./sources/cores.sh
+source ./sources/ansi-functions.sh
+source ./sources/questions.sh
+
+echo >install-workstation-log.txt
+
+# O figlet serve apenas para decorar o ShellScript
+echo Preparando Instalação
+sudo apt install -y figlet >/dev/null 2>&1
+# toda a saida da instalação do fliget está sendo direcionada para o /dev/null
+# tanto o stdout(>/dev/null) quanto o stderr(2>&1)
 
 write_log() {
   msg="$1"
-  echo $msg >>install-workstation-log.txt
-  echo $msg
+  echo "$msg" >>install-workstation-log.txt
+  echo "$msg"
 }
 
-source ./sources/cores.sh
-source ./sources/ansi-functions.sh
-echo >install-workstation-log.txt
+title() {
+  printf "$NEGRITO"
+  printf "$VERDE"
+  figlet "$1"
+  printf "$NORMAL"
+}
 
+tag() {
+  printf "${AZUL}"
+  printf "--------------------------------------------------------------------\n"
+  printf "${1}"
+  printf "--------------------------------------------------------------------"
+  printf "${NORMAL}\n"
+}
+
+title 'Update System'
 sudo apt update
 echo update - $? >>install-workstation-log.txt
 echo >>install-workstation-log.txt
 
+title 'Upgrade System'
 sudo apt upgrade -y
 echo status upgrade - $? >>install-workstation-log.txt
 echo >>install-workstation-log.txt
 
-sudo snap install code --classic
-echo status install code $? >>install-workstation-log.txt
-echo >>install-workstation-log.txt
-
+title "Install Curl"
 sudo apt install curl -y
 echo status install curl $? >>install-workstation-log.txt
 echo >>install-workstation-log.txt
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
-echo status install JetBrainsMono $? >>install-workstation-log.txt
-echo >>install-workstation-log.txt
+install_vscode() {
+  title "Install VsCode"
+  sudo snap install code --classic
+  echo status install code $? >>install-workstation-log.txt
+  echo >>install-workstation-log.txt
+}
+confirm_question "Deseja instalar o VsCode" "install_vscode" "break_line"
 
-source sources/git-install.sh
+install_jet_brains_mono() {
+  title "Install JetBrainsMono"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+  echo status install JetBrainsMono $? >>install-workstation-log.txt
+  echo >>install-workstation-log.txt
+}
+confirm_question "Deseja instalar a font JetBrainsMono" "install_jet_brains_mono" "break_line"
 
-source sources/docker_install.sh
+install_git() {
+  title "Install Git"
+  source sources/install-git.sh
+}
+confirm_question "Deseja instalar o Git" "install_git" "break_line"
+
+install_docker() {
+  title "Install Docker"
+  source sources/install-docker.sh
+}
+confirm_question "Deseja instalar o Docker" "install_docker" "break_line"
+
+install_zsh() {
+  title "Install Zsh"
+  source sources/install-zsh.sh
+}
+confirm_question "Deseja instalar o zsh" "install_zsh" "break_line"
+
+title "Restart the system"

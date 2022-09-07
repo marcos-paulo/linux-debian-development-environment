2bc24ffc546246e0677d75eeb1a11c7f8c3f0842
Marcos Paulo <marcospaulo.mpa@gmail.com>
2022-09-07 17:54:28 -0300

    :memo: Add TODO comments in workstation-install.sh

--- workstation-install.sh
+++ workstation-install.sh
@@ -10,13 +10,14 @@ echo Preparando Instalação
 sudo apt install -y figlet >/dev/null 2>&1
 # toda a saida da instalação do fliget está sendo direcionada para o /dev/null
 # tanto o stdout(>/dev/null) quanto o stderr(2>&1)
-
+# TODO refactore write_log to new file
 write_log() {
   msg="$1"
   echo "$msg" >>install-workstation-log.txt
   echo "$msg"
 }
 
+# TODO refactore title to new file
 title() {
   printf "$NEGRITO"
   printf "$VERDE"
@@ -24,6 +25,7 @@ title() {
   printf "$NORMAL"
 }
 
+# TODO refactore tag to new file
 tag() {
   printf "${AZUL}"
   printf "####################################################################\n"
@@ -32,6 +34,7 @@ tag() {
   printf "${NORMAL}\n"
 }
 
+# TODO refactore tag_question to new file
 tag_question() {
   printf "${AZUL}"
   printf "####################################################################\n"
@@ -52,43 +55,52 @@ sudo apt upgrade -y
 echo status upgrade - $? >>install-workstation-log.txt
 echo >>install-workstation-log.txt
 
+# TODO refactore install curl to new file
 title "Install Curl"
 sudo apt install curl -y
 echo status install curl $? >>install-workstation-log.txt
 echo >>install-workstation-log.txt
 
+# TODO refactore install vscode to new file
 install_vscode() {
   title "Install VsCode"
   sudo snap install code --classic
   echo status install code $? >>install-workstation-log.txt
   echo >>install-workstation-log.txt
 }
-confirm_question "Deseja instalar o VsCode" "install_vscode" "break_line"
+tag_question "confirm_question" "Deseja instalar o VsCode?" "install_vscode" "break_two_line"
 
+## TODO refactore install font jetbrainsmono font to install vscode file
 install_jet_brains_mono() {
   title "Install JetBrainsMono"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
   echo status install JetBrainsMono $? >>install-workstation-log.txt
   echo >>install-workstation-log.txt
 }
-confirm_question "Deseja instalar a font JetBrainsMono" "install_jet_brains_mono" "break_line"
+tag_question "confirm_question" "Deseja instalar a font JetBrainsMono?" "install_jet_brains_mono" "break_two_line"
 
+# TODO refactore extract function to install-git.sh file
 install_git() {
   title "Install Git"
   source sources/install-git.sh
 }
-confirm_question "Deseja instalar o Git" "install_git" "break_line"
+tag_question "confirm_question" "Deseja instalar o Git?" "install_git" "break_two_line"
 
+# TODO refactore extract function to "./sources/install-docker.sh" file
 install_docker() {
   title "Install Docker"
   source sources/install-docker.sh
 }
-confirm_question "Deseja instalar o Docker" "install_docker" "break_line"
+tag_question "confirm_question" "Deseja instalar o Docker?" "install_docker" "break_two_line"
 
+# TODO refactore extract function to "./sources/install-zsh.sh" file
 install_zsh() {
   title "Install Zsh"
   source sources/install-zsh.sh
 }
-confirm_question "Deseja instalar o zsh" "install_zsh" "break_line"
+tag_question "confirm_question" "Deseja instalar o zsh?" "install_zsh" "break_two_line"
 
+source sources/install-neovim.sh
+
+# TODO To implement: show cursor when canceling script execution
 title "Restart the system"

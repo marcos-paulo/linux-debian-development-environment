cd010b14bc5a6ab549b7b72bf391908305f0315c
Marcos Paulo <marcospaulo.mpa@gmail.com>
2022-09-07 20:03:07 -0300

    :memo: Add script install yarn in sources/install-node.sh

--- sources/install-node.sh
+++ sources/install-node.sh
@@ -3,5 +3,6 @@ install_nodejs() {
   title "Install Node"
   curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
   sudo apt-get install -y nodejs
+  sudo npm install --global yarn
 }
 tag_question "confirm_question" "Deseja instalar o NodeJs" "install_nodejs" "break_two_line"

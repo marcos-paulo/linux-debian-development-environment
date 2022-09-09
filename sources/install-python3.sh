08462b5d770d4798f2a94700e976f0967fe3264e
Marcos Paulo <marcospaulo.mpa@gmail.com>
2022-09-09 15:15:37 -0300

    :memo: Add title log in sources/install-python3.sh

--- sources/install-python3.sh
+++ sources/install-python3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 install_python3_pip() {
+  title "Install python3-pip"
   break_line
   sudo apt install -y python3-pip >install-python3.txt 2>&1
 }
-tag_question "confirm_question" "Deseja instalar o Python3-pip?" "install_python3" "break_two_line"
+tag_question "confirm_question" "Deseja instalar o Python3-pip?" "install_python3_pip" "break_two_line"

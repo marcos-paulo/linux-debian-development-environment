c0afe1778a8f924b30c3708ccf2293d9e00a3770
Marcos <marcospaulo.mpa@gmail.com>
2022-09-12 18:56:50 -0300

    :memo: Refactor path in imports.sh from renamed files:
    renamed:    sources/JetBrainsMono.sh -> util/JetBrainsMono.sh
    renamed:    sources/ansi-functions.sh -> util/ansi-functions.sh
    renamed:    sources/cores.sh -> util/cores.sh

--- util/ansi-functions.sh
+++ util/ansi-functions.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# $1 string de onde o cursor tem que ser movido
-move_cursor_to_first_column() {
-  # pega a quantidade de caracteres de $1
-  lenght_line=${#1}
-  # a move o cursor n colunas para a esquerda, onde n = $lenght_line
-  printf "\033[${lenght_line}D"
-}
-
 clear_line() {
   printf "\033[2K" # limpa a linha
 }

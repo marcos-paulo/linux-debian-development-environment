cdba26ef922fc7ab4e703efdec91bede897cad60
Marcos Paulo <marcospaulo.mpa@gmail.com>
2022-09-14 16:36:40 +0000

    :truck: Refactore file workstation-install.sh rename paths imports

--- workstation-install.sh
+++ workstation-install.sh
@@ -3,29 +3,29 @@ source "./imports.sh"
 
 echo >install-workstation-log.txt
 
-source "./sources/install-figlet.sh"
+source "./install/install-figlet.sh"
 
-source "./sources/install-update-system.sh"
+source "./install/install-update-system.sh"
 
-source "./sources/install-curl.sh"
+source "./install/install-curl.sh"
 
-source "./sources/install-vscode.sh"
+source "./install/install-vscode.sh"
 
-source "./sources/install-jet-brains-mono.sh"
+source "./install/install-jet-brains-mono.sh"
 
-source "./sources/install-git.sh"
+source "./install/install-git.sh"
 
-source "./sources/install-docker.sh"
+source "./install/install-docker.sh"
 
-source "./sources/install-zsh.sh"
+source "./install/install-zsh.sh"
 
-source "./sources/install-node.sh"
+source "./install/install-node.sh"
 
-source "./sources/install-python3.sh"
+source "./install/install-python3.sh"
 
-source "./sources/install-rust.sh"
+source "./install/install-rust.sh"
 
-source "./sources/install-neovim.sh"
+source "./install/install-neovim.sh"
 
 tag_figlet "Restart the system"
 break_line
